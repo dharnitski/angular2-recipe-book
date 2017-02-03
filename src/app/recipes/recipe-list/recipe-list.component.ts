7a19537b4ddf86847ce76cef8fcff530a94e936c
Dmitry Harnitski <dharnitski@gmail.com>
2017-02-02 20:30:10 -0500

    add hardcoded Recipes collection

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,9 +7,14 @@ import { Recipe } from '../recipe';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('Schnitzel', 'Schnitzel', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg', []),
+    new Recipe('Summer Salad', 'Summer Salad', 'http://cdn.iowagirleats.com/wp-content/uploads/2013/05/Triple-Berry-Summer-Salad-03_mini.jpg', []),
+    new Recipe('Ice Cream', 'Ice Cream', 'http://lunar.moonworks.pl/upload/images/ice-cream.jpg', []),
+    new Recipe('Apple Pie', 'Apple Pie', 'http://ak.c.ooyala.com/8xanByNDE6hb53ycG6CyupljeIY9O_xa/promo295046062', []),
+    new Recipe('Grilled Chicken', 'Grilled Chicken', 'https://d1doqjmisr497k.cloudfront.net/~/media/Recipe-Photos/McCormick/Main-Dishes/1007x545/Zesty-Herb-Grilled-Chicken.ashx', [])
+  ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  recipe = new Recipe('Dummy', 'Dummy', 'http://tekclasses.com/wp-content/uploads/2016/02/AngularJS-Shield-large-282x300.png')
 
   constructor() { }
 
