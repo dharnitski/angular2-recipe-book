4f536b4c000a8c97624ae5cfb56e2304c38a53c0
Dmitry Harnitski <dharnitski@gmail.com>
2017-02-13 22:36:33 -0500

    fetch active recipe

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,14 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Rx';
+import { RecipeService } from './../recipe.service';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'rb-recipe-edit',
   templateUrl: './recipe-edit.component.html'
 })
-export class RecipeEditComponent implements OnInit {
+export class RecipeEditComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
+  private recipeIndex: number;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService) { }
 
   ngOnInit() {
+    let isNew = true;
+    this.subscription = this.route.params.subscribe(
+      (params: any) => {
+        this.recipeIndex = +params['id'];
+        if (params.hasOwnProperty('id')) {
+          isNew = false;
+        } else {
+          isNew = true;
+        }
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
