7273f9fc4fcb714260f953326a9a1338da2323cb
Dmitry Harnitski <dharnitski@gmail.com>
2017-02-19 00:35:57 -0500

    Using a Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
+import { CoreModule } from './core.module';
 import { routing } from './app.routing';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeService } from './recipes/recipe.service';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
-import { DropdownDirective } from './dropdown.directive';
-import { HomeComponent } from './home.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    DropdownDirective,
-    HomeComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    routing
+    routing,
+    CoreModule
   ],
   providers: [RecipeService, ShoppingListService],
   bootstrap: [AppComponent]
